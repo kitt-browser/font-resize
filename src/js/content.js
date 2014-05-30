a0be13d56692d44d70a84c841e1db7a6cea3fe1b
Tomas Brambora <tomas.brambora@gmail.com>
2014-05-30 14:51:58 +1000

    Destroying popup on hide.

--- src/js/content.js
+++ src/js/content.js
@@ -3,7 +3,6 @@
 
 
   var $ = require('../vendor/jquery/jquery');
-  var template = require('../html/popover.jade');
   var _ = require('../vendor/underscore/underscore');
   require('../vendor/flowtype/flowtype');
 
@@ -11,52 +10,67 @@
   require('../vendor/jquery-ui-scalebreaker/jq-scalebreaker.css');
   require('../vendor/jquery-ui-scalebreaker/jq-scalebreaker');
 
-  require('../css/content.css');
-
   require('./../vendor/nouislider/jquery.nouislider.min');
   require('./../vendor/nouislider/jquery.nouislider.css');
 
+  require('../css/content.css');
 
   var _jQuery = $.noConflict(true);
 
 
   (function ($) {
 
-    var $modal;
-
-    chrome.runtime.onMessage.addListener(
-      function(request, sender, sendResponse) {
-        console.log('message received:', request.event);
-        $('body').scalebreaker('show');
-      });
-
-    $('body').scalebreaker({
-      dialogContent: '<div class="scalebreaker-123-slider"/>',
-      dialogPosition: 'bottom'
-    });
-
     var ratioLimits = {
       min: 0,
       max: 80
     };
-    var ratio = (ratioLimits.min + ratioLimits.max) / 2;
 
-    var $slider = $(".scalebreaker-123-slider");
-    $slider.noUiSlider({
-      start: [ratio],
-      range: {
-        'min': [ratioLimits.min],
-        'max': [ratioLimits.max]
-      }
-    });
+    function showPopup() {
+      var ratio = (ratioLimits.min + ratioLimits.max) / 2;
 
-    $slider.on('slide', function() {
-      ratio = $slider.val();
-      console.log('new font ratio', ratio);
-      $('body').flowtype({
-        fontRatio: (ratioLimits.max - ratio)
+      $('body').scalebreaker('show');
+
+      $('body').on('dialogHidden.jq-scalebreaker', function() {
+        setTimeout(function() {
+          $('body').scalebreaker('destroy');
+        }, 200);
       });
-    });
+
+      var $slider = $('body').scalebreaker('getContentElement')
+        .find(".scalebreaker-123-slider");
+      console.log('starting with ratio', ratio);
+      $slider.noUiSlider({
+        start: [ratio],
+        range: {
+          'min': [ratioLimits.min],
+          'max': [ratioLimits.max]
+        }
+      });
+
+      $slider.on('slide', function() {
+        ratio = $slider.val();
+        console.log('new font ratio', ratio);
+        $('body').flowtype({
+          fontRatio: (ratioLimits.max - ratio)
+        });
+      });
+    }
+
+    chrome.runtime.onMessage.addListener(
+      function(request, sender, sendResponse) {
+        console.log('message received:', request.cmd);
+
+        $('body').scalebreaker({
+          dialogContent: '<div class="scalebreaker-123-slider"/>',
+          dialogPosition: 'bottom'
+        });
+
+        _.delay(function() {
+          showPopup();
+        }, 20);
+      });
+
+
 
   })(_jQuery);
 
--- src/vendor/jquery-ui-scalebreaker/jq-scalebreaker.js
+++ src/vendor/jquery-ui-scalebreaker/jq-scalebreaker.js
@@ -9,104 +9,166 @@
         dialogPosition: 'bottom',
         closeOnBackdrop: true,
         denyUserScroll: true,
+        refreshOnScroll: true,
+        mobileFriendlyInitialWidth: 320,
+        mobileFriendlyMaxWidth: 568,
+        broadcastEvents: true,
         debug: false
       },
       _create: function() {
-        this.rawElement = "<div id='" + this.options.idNamespace + "'>           <div id='" + this.options.idNamespace + "-dialog'>             <div id='" + this.options.idNamespace + "-dialog-content'></div>           </div>         </div>";
-        this.backdrop = null;
+        this.rawElement = "<div id='" + this.options.idNamespace + "-wrapper'>          <div id='" + this.options.idNamespace + "-dialog-scalable'>            <div id='" + this.options.idNamespace + "-dialog-scrollable'>              <div id='" + this.options.idNamespace + "-dialog-content'></div>              <span id='" + this.options.idNamespace + "-dialog-close'></span>            </div>          </div>        </div>";
+        this.scrollbar = null;
+        this.wrapper = null;
         this.dialog = null;
-        this.dialogContent = null;
+        this.scrollarea = null;
+        this.content = null;
+        this.close = null;
+        this.fullPageDimensions = {};
         this.scaleFactor = null;
-        this.fullPageHeight = null;
-        this.currentViewportOffset = [];
-        return this._initWidget();
+        this.currentViewportOffset = null;
+        this.isMobileBrowser = /iPhone|iPod|Android|BlackBerry/.test(navigator.userAgent);
+        this.state = 'hidden';
+        this._initWidget();
+        return this._logMessage('widget created', this.wrapper);
       },
       _initWidget: function() {
         $('body').append(this.rawElement);
-        this.backdrop = $('#' + this.options.idNamespace);
-        this.dialog = $('#' + this.options.idNamespace + '-dialog');
-        this.dialogContent = $('#' + this.options.idNamespace + '-dialog-content');
-        this._logMessage('wrapper reference created', this.backdrop);
-        this._setBackdropHeight();
-        return this.addContentToDialog(this.options.dialogContent);
+        this.wrapper = $('#' + this.options.idNamespace + '-wrapper');
+        this.dialog = $('#' + this.options.idNamespace + '-dialog-scalable');
+        this.scrollarea = $('#' + this.options.idNamespace + '-dialog-scrollable');
+        this.content = $('#' + this.options.idNamespace + '-dialog-content');
+        this.close = $('#' + this.options.idNamespace + '-dialog-close');
+        return this.changeDialogContent(this.options.dialogContent);
       },
-      _logMessage: function(name, args) {
-        if (this.options.debug) {
-          return console.debug("" + this.options.idNamespace + ": " + name, args);
+      _setWrapperDimensions: function() {
+        var bodyInlineStyle;
+        bodyInlineStyle = $('body').attr('style');
+        $('body').css({
+          'overflow': 'hidden'
+        });
+        this.fullPageDimensions.width = Math.max(document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);
+        this.fullPageDimensions.height = Math.max(document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+        if (this.isMobileBrowser) {
+          this.wrapper.css({
+            'width': this.fullPageDimensions.width,
+            'height': this.fullPageDimensions.height
+          });
+        } else {
+          this.wrapper.css({
+            'height': this.fullPageDimensions.height
+          });
+        }
+        if (bodyInlineStyle) {
+          return $('body').attr('style', bodyInlineStyle);
+        } else {
+          return $('body').removeAttr('style');
         }
       },
-      _getScaleFactor: function() {
-        this.scaleFactor = window.innerWidth / document.documentElement.clientWidth;
+      _getCurrentViewport: function() {
+        this.scaleFactor = window.innerWidth / this.fullPageDimensions.width;
         this._logMessage('scale factor', this.scaleFactor);
-        return this.scaleFactor;
-      },
-      _getCurrentViewportOffset: function() {
         this.currentViewportOffset = [window.pageXOffset, window.pageYOffset];
-        this._logMessage('current viewport offset', this.currentViewportOffset);
-        return this.currentViewportOffset;
+        return this._logMessage('current viewport offset', this.currentViewportOffset);
       },
-      _setBackdropHeight: function() {
-        this.fullPageHeight = Math.max(document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
-        return this.backdrop.css({
-          'height': this.fullPageHeight
-        });
-      },
-      addContentToDialog: function(content) {
-        this.dialogContent.html(content);
-        return this._logMessage('adding content to dialog', content);
-      },
-      rescaleAndReposition: function(el) {
-        var newViewport, _self;
-        _self = this;
-        newViewport = this._getCurrentViewportOffset();
+      _rescaleAndReposition: function() {
+        var mobileFriendlyScaleFactor;
+        if (!this.isMobileBrowser) {
+          this.dialog.css({
+            'left': this.currentViewportOffset[0]
+          });
+        } else if (this.isMobileBrowser && (this.fullPageDimensions.width > this.options.mobileFriendlyMaxWidth)) {
+          mobileFriendlyScaleFactor = this.fullPageDimensions.width / this.options.mobileFriendlyInitialWidth;
+          this.dialog.css({
+            'width': this.options.mobileFriendlyInitialWidth,
+            'left': this.currentViewportOffset[0],
+            'transform': "scale(" + (this.scaleFactor * mobileFriendlyScaleFactor) + ")",
+            '-webkit-transform': "scale(" + (this.scaleFactor * mobileFriendlyScaleFactor) + ")"
+          });
+        } else {
+          this.dialog.css({
+            'left': this.currentViewportOffset[0],
+            'transform': "scale(" + this.scaleFactor + ")",
+            '-webkit-transform': "scale(" + this.scaleFactor + ")"
+          });
+        }
         if (this.options.dialogPosition === 'top') {
-          el.css({
-            'top': newViewport[1],
-            'left': newViewport[0],
+          this.dialog.css({
+            'top': this.currentViewportOffset[1],
             'transform-origin': '0 0',
             '-webkit-transform-origin': '0 0'
           });
         }
         if (this.options.dialogPosition === 'bottom') {
-          el.css({
-            'bottom': _self.fullPageHeight - (newViewport[1] + window.innerHeight),
-            'left': newViewport[0],
+          return this.dialog.css({
+            'bottom': this.fullPageDimensions.height - (this.currentViewportOffset[1] + window.innerHeight),
             'transform-origin': '0 100%',
             '-webkit-transform-origin': '0 100%'
           });
         }
-        return el.css({
-          'transform': "scale(" + (_self._getScaleFactor()) + ")",
-          '-webkit-transform': "scale(" + (_self._getScaleFactor()) + ")"
-        });
+      },
+      _manageScrollbar: function() {
+        if (this.content.outerHeight() > this.scrollarea.outerHeight() && !this.scrollbar) {
+          return this.scrollbar = new IScroll(this.scrollarea.get(0), {
+            HWCompositing: true,
+            useTransition: false,
+            click: true
+          });
+        } else if (this.scrollbar) {
+          return this.scrollbar.refresh();
+        }
+      },
+      _triggerEvent: function(name, data) {
+        this.element.trigger(name, [data]);
+        return this._logMessage(name, data);
+      },
+      _logMessage: function(name, args) {
+        if (this.options.debug) {
+          return console.log("jq-scalebreaker: " + name, args);
+        }
       },
       show: function() {
         var _self;
         _self = this;
         if (this.options.closeOnBackdrop) {
-          _self.backdrop.on("click." + this.options.idNamespace, function(e) {
-            if (e.target === _self.backdrop.get(0)) {
+          _self.wrapper.on("click." + this.options.idNamespace, function(e) {
+            if (e.target === _self.wrapper.get(0)) {
               return _self.hide();
             }
           });
         }
+        this.close.on("click." + this.options.idNamespace, function(e) {
+          return _self.hide();
+        });
         if (this.options.denyUserScroll) {
           $('body').on("touchmove." + this.options.idNamespace, function(e) {
             return e.preventDefault();
           });
         }
-        this.backdrop.addClass("" + this.options.idNamespace + "-show");
-        this.rescaleAndReposition(this.dialog);
+        this.wrapper.addClass("" + this.options.idNamespace + "-show");
+        this.refresh();
         if (this.options.cssAnimated) {
-          this.backdrop.addClass("" + this.options.idNamespace + "-animate-in");
-          this.backdrop.on('animationend webkitAnimationEnd', function(e) {
-            if (e.target === _self.dialogContent.get(0)) {
-              _self.backdrop.removeClass("" + _self.options.idNamespace + "-animate-in");
-              return _self.backdrop.off('animationend webkitAnimationEnd');
+          this.wrapper.addClass("" + this.options.idNamespace + "-animate-in");
+          this.wrapper.on('animationend webkitAnimationEnd', function(e) {
+            if (e.target === _self.scrollarea.get(0)) {
+              _self.wrapper.removeClass("" + _self.options.idNamespace + "-animate-in");
+              _self.wrapper.off('animationend webkitAnimationEnd');
+              this.state = 'shown';
+              if (this.options.broadcastEvents) {
+                return this._triggerEvent("dialogShown." + this.options.idNamespace, this.wrapper);
+              }
             }
           });
+        } else {
+          this.state = 'shown';
+          if (this.options.broadcastEvents) {
+            this._triggerEvent("dialogShown." + this.options.idNamespace, this.wrapper);
+          }
         }
-        return this._logMessage('showing widget');
+        if (this.options.refreshOnScroll) {
+          return $(window).on("scroll." + this.options.idNamespace, function(e) {
+            return _self.refresh();
+          });
+        }
       },
       hide: function() {
         var _self;
@@ -115,34 +177,67 @@
           $('body').off("touchmove." + this.options.idNamespace);
         }
         if (this.options.closeOnBackdrop && this.options.cssAnimated) {
-          _self.backdrop.off("click." + this.options.idNamespace);
-          this.backdrop.addClass("" + this.options.idNamespace + "-animate-out");
-          this.backdrop.on('animationend webkitAnimationEnd', function(e) {
-            if (e.target === _self.dialogContent.get(0)) {
-              _self.backdrop.removeClass("" + _self.options.idNamespace + "-animate-out");
-              _self.backdrop.removeClass("" + _self.options.idNamespace + "-show");
-              _self.dialog.removeAttr('style');
-              return _self.backdrop.off('animationend webkitAnimationEnd');
+          _self.wrapper.off("click." + this.options.idNamespace);
+          this.wrapper.addClass("" + this.options.idNamespace + "-animate-out");
+          this.wrapper.on('animationend webkitAnimationEnd', function(e) {
+            if (e.target === _self.scrollarea.get(0)) {
+              _self.wrapper.removeClass("" + _self.options.idNamespace + "-animate-out");
+              _self.wrapper.removeClass("" + _self.options.idNamespace + "-show");
+              _self.wrapper.off('animationend webkitAnimationEnd');
+              this.state = 'hidden';
+              if (this.options.broadcastEvents) {
+                return this._triggerEvent("dialogHidden." + this.options.idNamespace, this.wrapper);
+              }
             }
           });
         } else if (this.options.closeOnBackdrop) {
-          _self.backdrop.off("click." + this.options.idNamespace);
-          this.backdrop.removeClass("" + this.options.idNamespace + "-show");
-          this.dialog.removeAttr('style');
+          _self.wrapper.off("click." + this.options.idNamespace);
+          this.wrapper.removeClass("" + this.options.idNamespace + "-show");
+          this.state = 'hidden';
+          if (this.options.broadcastEvents) {
+            this._triggerEvent("dialogHidden." + this.options.idNamespace, this.wrapper);
+          }
         }
-        return this._logMessage('hiding widget');
+        if (this.options.refreshOnScroll) {
+          return $(window).off("scroll." + this.options.idNamespace);
+        }
       },
-      destroy: function() {
-        this.backdrop.remove();
+      changeDialogContent: function(content) {
+        this.content.html(content);
+        this.refresh();
+        return this._logMessage('adding content to dialog', content);
+      },
+      getContentElement: function() {
+        return this.content;
+      },
+      getDialogState: function() {
+        return this.state;
+      },
+      refresh: function() {
+        this._setWrapperDimensions();
+        this._getCurrentViewport();
+        this._rescaleAndReposition();
+        this._manageScrollbar();
+        return this._logMessage('refreshing');
+      },
+      _destroy: function() {
+        $(window).off("scroll." + this.options.idNamespace);
+        this.wrapper.remove();
         this.rawElement = null;
-        this.backdrop = null;
+        this.wrapper = null;
         this.dialog = null;
+        this.scrollarea = null;
+        this.content = null;
+        this.close = null;
         this.scaleFactor = null;
-        this.initialViewport = null;
+        this.fullPageDimensions = null;
         this.currentViewportOffset = null;
-        return this._destroy();
-      },
-      _destroy: $.noop
+        if (this.scrollbar) {
+          this.scrollbar.destroy();
+        }
+        this.scrollbar = null;
+        return this._logMessage('widget instance destroyed');
+      }
     });
   })(jQuery);
 
